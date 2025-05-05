4f35526d16e2d904b890a4f491002022cf54c3d9
udedesire <udesire20@gmail.com>
2025-05-05 11:50:41 +0100

    new commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("de").innerHTML = "Paragraph changed.";
   }
+  let a, b, c;  // Declare 3 variables
+    a = 5;        // Assign the value 5 to a
+    b = 6;        // Assign the value 6 to b
+    c = a + b;    // Assign the sum of a and b to c
+    document.getElementById("demo1").innerHTML = c;
