03acbc61cab11b4b12b75076fe5a3e1fdaccb692
udedesire <udesire20@gmail.com>
2025-05-12 15:13:01 +0100

    new commit

--- Js by bro code/index.js
+++ Js by bro code/index.js
@@ -1,2 +1,15 @@
-document.getElementById("myh1").textContent = 'Hello';
-document.getElementById("myp").innerHTML = 'I love pizza';
+// document.getElementById("myh1").textContent = 'Hello';
+// document.getElementById("myp").innerHTML = 'I love pizza';
+
+let x;
+x = 100;
+console.log(x);
+
+let age = 25;
+let gpa = 3.5;
+let price = 25.55
+
+console.log(typeof age);
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is: ${gpa}`);
