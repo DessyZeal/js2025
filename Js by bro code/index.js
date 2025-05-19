b7cbdffba8affd0869a3c8052f3a16f846a2f659
udedesire <udesire20@gmail.com>
2025-05-19 13:52:12 +0100

    new commit

--- Js by bro code/index.js
+++ Js by bro code/index.js
@@ -1,15 +1,46 @@
-// document.getElementById("myh1").textContent = 'Hello';
-// document.getElementById("myp").innerHTML = 'I love pizza';
+document.getElementById("myh1").textContent = 'Hello';
+document.getElementById("myp").innerHTML = 'I love pizza';
 
-let x;
+// declaration of a variable
+let x; 
+// assigning a value to the variable
 x = 100;
 console.log(x);
 
-let age = 25;
+// Datatype Number
+let age1 = 25;
 let gpa = 3.5;
-let price = 25.55
+let price = 25.55;
 
-console.log(typeof age);
-console.log(`You are ${age} years old`);
+// Datatype String
+let firstName = "Desire";
+let favFood = "Rice";
+let email = "ude12@example.com"
+
+// Datatype Boolean
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(typeof online);
+console.log(`${firstName} is online: ${online}`);
+console.log(`Is this car for sale? ${forSale}`);
+console.log(`is ${firstName} a student? ${isStudent}`);
+
+console.log(typeof firstName);
+console.log(`You name is ${firstName}`);
+console.log(`My favourite food is ${favFood}`);
+console.log(`my email is ${email}`);
+
+console.log(typeof age1);
+console.log(`You are ${age1} years old`);
 console.log(`The price is ${price}`);
 console.log(`Your gpa is: ${gpa}`);
+
+let fullName = "Ude Desire";
+let age = 21;
+let student = true;
+
+document.getElementById("p1").innerHTML = `My name is ${fullName}`;
+document.getElementById("p2").innerHTML = `I am ${age} yrs old`;
+document.getElementById("p3").innerHTML = `Enrolled: ${student}`;
