f370c405b80e5fb163c2a0cfdde5c8b3658efd92
udedesire <udesire20@gmail.com>
2025-06-04 04:46:48 +0100

    new commit

---
+++ js arrays/script.js
@@ -0,0 +1,8 @@
+const Favmovies = ["frozen", "moana", "tinkerbell"];
+// remove the last item on the array
+Favmovies.pop();
+// add a new movie to the end of the array
+Favmovies.push("migration");
+// check array length
+Favmovies.length;
+console.log(Favmovies);
