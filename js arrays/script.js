af51fe5e1c43153c07925a95bdf7f792976e7d15
udedesire <udesire20@gmail.com>
2025-06-04 12:23:29 +0100

    new commit

--- js arrays/script.js
+++ js arrays/script.js
@@ -6,5 +6,16 @@ Favmovies.push("migration");
 // check array length
 Favmovies.length;
 // Favmovies[2];
-// console.log(Favmovies.toString());
-console.log(Array.isArray(Favmovies));
+console.log(Favmovies.toString());
+// console.log(Array.isArray(Favmovies));
+
+
+const MyGirls = ["Joy", "Love", "Peace"];
+const MyBoys = ["David", "Daniel"];
+// to concatenate two arrays
+const MyChildren = MyGirls.concat(MyBoys);
+console.log(MyChildren);
+// js array search
+console.log(Favmovies.indexOf("moana"));
+console.log(Favmovies.includes("tinkerbell"));
+
