45a73a2c43e82c89c43a88b03a8318af940f526e
udedesire <udesire20@gmail.com>
2025-06-10 11:19:18 +0100

    new commit

--- js conditionals/script.js
+++ js conditionals/script.js
@@ -1,6 +1,34 @@
 let isStudent = false;
-if (!isStudent) {
+
+if (isStudent) {
     console.log("I Love school")
 } else {
     console.log("I hate school")
+}
+// switch
+let day = "Tuesday";
+
+switch (day) {
+    case "Monday":
+        console.log("Start of the week");
+        break;
+    case "Friday":
+        console.log("Almost weekend");
+        break;
+    default:
+        console.log("Just another day");
+}
+// exercise on switch
+let days = "Sunday";
+
+switch (days) {
+    case "Sunday":
+        console.log("Weekend");
+        break;
+    case "Friday":
+        console.log("Weekday");
+        break;
+    default:
+        console.log("No day");
+        break;
 }
