43e989abae085d47effad4b02e4bf04c0ac6dfd7
Ude Desire <udesire20@gmail.com>
2025-09-27 16:11:16 +0100

    javascript recap

---
+++ recapjs.js
@@ -0,0 +1,17 @@
+// Variables and Data Types
+let userName = "Ude Desire";
+let age = 22;
+let loveCoding = true;
+
+console.log("My name is " + userName + ", I am " + age + " years old and I love coding: " + loveCoding);
+
+// excercise 2
+let favFoods = ["Rice", "Beans", "Yam", "Plantain"];
+let userDetails = {
+    name: userName,
+    age: age,
+    loveCoding: loveCoding,
+    favFoods: favFoods
+};
+
+console.log(userDetails);
