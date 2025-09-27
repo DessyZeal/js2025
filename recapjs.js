899d3ff15384570af6d1dc2a62064e66736629a5
Ude Desire <udesire20@gmail.com>
2025-09-27 16:25:48 +0100

    javascript recap

--- recapjs.js
+++ recapjs.js
@@ -1,4 +1,5 @@
 // Variables and Data Types
+// exercise 1
 let userName = "Ude Desire";
 let age = 22;
 let loveCoding = true;
@@ -13,5 +14,32 @@ let userDetails = {
     loveCoding: loveCoding,
     favFoods: favFoods
 };
-
+userDetails.age += 1; // Increment age by 1
 console.log(userDetails);
+
+// exercise 3
+// banking system
+let accounts = {
+    owner: "John Doe",
+    balance: 12000,
+    isActive: false
+};
+accounts.balance += 500;
+accounts.isActive = true;
+
+console.log(accounts);
+
+// exercise 4
+let student = {
+    nameStudent: "Jane Doe",
+    subjects: ["English", "Math", "Sciences"],
+    grades: {
+        English: "A",
+        Math: "B",
+        Sciences: "C"
+    }
+};
+student.Sciences = "A";
+student.History = "B";
+
+console.log(student);
