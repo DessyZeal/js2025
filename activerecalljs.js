6b0977f9182ce4c72c7eb55813878de20fa7822c
Ude Desire <udesire20@gmail.com>
2025-10-20 11:26:40 +0100

    Tailwind Dashboard

---
+++ activerecalljs.js
@@ -0,0 +1,12 @@
+// Variables and DataType
+let nameZ = "Zaya Coder";
+let ageZ = 7;
+let favColorZ = "Green";
+console.log(nameZ + " is " + ageZ + " years old and likes " + favColorZ + ".");
+
+// create 3 variables with numbers and calculate their average
+let num1 = 8;
+let num2 = 12;
+let num3 = 20;
+let average = (num1 + num2 + num3) % 3;
+console.log("The average of " + num1 + ", " + num2 + ", and " + num3 + " is " + average + ".");
