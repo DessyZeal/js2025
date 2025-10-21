0123bc3fcc68de6e099199ee2b540630ffdddd0b
Ude Desire <udesire20@gmail.com>
2025-10-21 11:36:20 +0100

    javascript recap

--- jsDOM.js
+++ jsDOM.js
@@ -1,3 +1,11 @@
+// finding html elements by Id
 const greeting = document.getElementById("greeting").innerHTML = "Hello World!";
 const intro = document.getElementById("intro").textContent = "Introduction to DOM in Javascript";
 console.log(typeof(intro));
+// finding html elements by tagName
+const headertwo = document.getElementsByTagName("h2");
+// changes the color of the first h2
+headertwo[0].style.color = "blue";
+// changes the content in the second h2
+headertwo[1].innerHTML = "Codwiz has changed my life"
+
