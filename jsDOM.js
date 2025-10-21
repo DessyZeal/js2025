e8d3d5244824f4564479167a57847faabf7fe25e
Ude Desire <udesire20@gmail.com>
2025-10-21 11:16:28 +0100

    javascript recap

--- jsDOM.js
+++ jsDOM.js
@@ -1,1 +1,3 @@
 const greeting = document.getElementById("greeting").innerHTML = "Hello World!";
+const intro = document.getElementById("intro").textContent = "Introduction to DOM in Javascript";
+console.log(typeof(intro));
